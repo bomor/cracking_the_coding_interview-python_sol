a935fb58edbd2d28f22dfc91490307debe9e7be8
Mor Boani <32219770+bomor@users.noreply.github.com>
2017-10-14 17:22:06 +0300

    Update q2_2.py

--- chapter2/q2_2.py
+++ chapter2/q2_2.py
@@ -3,15 +3,15 @@ from node import Node, create_list
 
 def nth_to_last(head, n):
 	if n < 0:
-		return None
+		return
 	p1 = head
 	p2 = head
 	for i in xrange(n):
 		# in case that list size < n
 		if not p2:
-			return None
+			return
 		p2 = p2.next
-	while p2.next != None:
+	while p2.next:
 		p1 = p1.next
 		p2 = p2.next
 	return p1
